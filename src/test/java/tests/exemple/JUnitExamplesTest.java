64f4541f500190d9c2a185105b3fba566a36be29
Damdam <sadpirozhok@gmail.com>
2026-02-18 19:46:00 +0300

    ДОбавили

---
+++ src/test/java/tests/exemple/JUnitExamplesTest.java
@@ -0,0 +1,19 @@
+package tests.exemple;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class JUnitExamplesTest {
+    @BeforeEach
+    void startBrowser() {
+        System.out.println ("browser has started");
+    }
+    @Test
+    void firstTest(){
+        System.out.println("firstTest");
+    }
+    @Test
+    void secondTest() {
+        System.out.println("secondTest");
+    }
+}
